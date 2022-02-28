acd0660d5f97e68d958750a7f948201e254bf01b
guri-suhyun <myksh0903@naver.com>
2022-02-28 17:16:06 +0900

    Method_allfit_Sequential 수정

--- 12_Method_allfit_Sequential.py
+++ 12_Method_allfit_Sequential.py
@@ -6,117 +6,135 @@ from tqdm import tqdm
 import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
-from tensorflow.keras.layers import SimpleRNN
-from tensorflow.keras.layers import Dropout, InputLayer, Activation
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.optimizers import Adam
-from tensorflow.compat.v1.keras.layers import CuDNNLSTM
+# from tensorflow.keras.models import Sequentiasl
+# from tensorflow.keras.layers import Dense, LSTM
+# from tensorflow.keras.layers import SimpleRNN
+# from tensorflow.keras.layers import Dropout, InputLayer, Activation
+# from tensorflow.keras.callbacks import EarlyStopping
+# from tensorflow.keras.regularizers import l2
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.compat.v1.keras.layers import CuDNNLSTM
 from sklearn import metrics 
 
 import warnings
 warnings.filterwarnings(action='ignore')
 
-#한글설정
-import matplotlib.font_manager as fm
-
-font_dirs = ['/usr/share/fonts/truetype/nanum', ]
-font_files = fm.findSystemFonts(fontpaths=font_dirs)
-
-for font_file in font_files:
-    fm.fontManager.addfont(font_file)
-    
-# 한글 출력을 위해서 폰트 옵션을 설정합니다.
-# "axes.unicode_minus" : 마이너스가 깨질 것을 방지
-
-sns.set(font="NanumBarunGothic",
-        rc={"axes.unicode_minus":False},
-        style='darkgrid')
-
 #GPU 사용 설정, -1이면 CPU 사용
-os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
+#https://www.tensorflow.org/guide/gpu#allowing_gpu_memory_growth
+#프로세스의 요구량만큼 메모리 사용 설정
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print(gpus)
-if gpus:  # gpu가 있다면, 용량 한도를 5GB로 설정
-    tf.config.experimental.set_virtual_device_configuration(gpus[0], 
-                                                            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10*1024)])
-
+if gpus:
+    try:
+        for i in range(len(gpus)):
+            tf.config.experimental.set_memory_growth(gpus[i], True)
+    except RuntimeError as e:
+        # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
+        print(e)
 
 import random    
 seed_num = 42
 random.seed(seed_num)
-
+#-----데이터 로드-----
 X = np.load('/project/LSH/x_(7727,10,4068).npy')
 y = np.load('/project/LSH/y_(7727,1).npy')
+#-----Item name 로드-----
 
-model = tf.keras.models.load_model('./models/ALLFIT_01-0.4865.hdf5')
 COLS = list(pd.read_csv('/project/LSH/total_data_7727.csv')['ITEMID'].sort_values().unique())
+#-----각 feature의 Entropy를 기록한 dictionary 생성-----
+def entropy(ratio_list):
+    one_ratio, zero_ratio = ratio_list[0], ratio_list[1] 
+    return - ((one_ratio * (np.log2(one_ratio))) + (zero_ratio * (np.log2(zero_ratio))))
 
-with tf.device('/device:GPU:0'): 
-    result = []
-    for i in tqdm(range(X.shape[2])):
-        save_cols = X[:,:,i].copy()
-        #-----zero2one-----
-        X[:,:,i] = 1
-        pred1 = model.predict(X)
-        mean_pred1 = np.mean(pred1)
-        #-----one2zero-----
-        X[:,:,i] = 0
-        pred2 = model.predict(X)
-        mean_pred2 = np.mean(pred2)
+X = np.load('/project/LSH/x_(7727,10,4068).npy')
 
-        result.append({'feature' : str(COLS[i]), 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
-                       'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
+entropy_dict = {}
+for i in tqdm(range(len(COLS))):
+    one_ratio = X[:,:,i].sum() / (X.shape[0]*X.shape[1])
+    zero_ratio = 1 - one_ratio
+    entropy_num = entropy([one_ratio, zero_ratio])
+    entropy_dict[COLS[i]] = entropy_num
 
-        #값 복원
-        X[:,:,i] = save_cols
+
+#-----FI 측정 시작-----
+# with tf.device('/device:GPU:1'): 
+#     #-----모델 로드-----
+#     model = tf.keras.models.load_model('./models/ALLFIT_17-0.7645.hdf5')
+#     #-----RUN-----
+#     result = []
+#     for i in tqdm(range(X.shape[2])):
+#         save_cols = X[:,:,i].copy()
+#         #-----zero2one-----
+#         X[:,:,i] = 1
+#         pred1 = model.predict(X)
+#         mean_pred1 = np.mean(pred1)
+#         #-----one2zero-----
+#         X[:,:,i] = 0
+#         pred2 = model.predict(X)
+#         mean_pred2 = np.mean(pred2)
+
+#         result.append({'feature' : str(COLS[i]), 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
+#                        'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
+
+#         #값 복원
+#         X[:,:,i] = save_cols
         
-df = pd.DataFrame(result)
-df.to_csv('./data/Method_allfit.csv', index=False)
+# df = pd.DataFrame(result)
+# df.to_csv('./data/Method_allfit.csv', index=False)
 
 
-print('----------------end----------------')
+print('----------------Sequential----------------')
 
-# top10 = []
-# for n in range(10):
-#     if len(top10) > 1:
-#         print(top10)
-#         result = []
-#         for i in tqdm(range(X.shape[2])):
-#             save_cols = X[:,:,top10].copy()
-#             #-----zero2one-----
-#             X[:,:,top10] = 1
-#             pred1 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
-#             mean_pred1 = np.mean(pred1)
-#             #-----one2zero-----
-#             X[:,:,top10] = 0
-#             pred2 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
-#             mean_pred2 = np.mean(pred2)
-#             result.append({'feature_index' : i, 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
-#                            'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
-#         df = pd.DataFrame(result).sort_values('lambda0', ascending=False)
-#         top10.append(df.feature_index[n])
-#     else:
-#         result = []
-#         for i in tqdm(range(X.shape[2])):
-#             save_cols = X[:,:,i].copy()
-#             #-----zero2one-----
-#             X[:,:,i] = 1
-#             pred1 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
-#             mean_pred1 = np.mean(pred1)
-#             #-----one2zero-----
-#             X[:,:,i] = 0
-#             pred2 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
-#             mean_pred2 = np.mean(pred2)
+#-----FI 측정 시작-----
+with tf.device('/device:GPU:1'): 
+    #-----모델 로드-----
+    model = tf.keras.models.load_model('./models/ALLFIT_17-0.7645.hdf5')
+    top10 = []
+    top10_result = []
+    for n in range(2):
+        result = []
+        if len(top10) > 1:
+            print('####################  top10 :', top10)
+            for i in tqdm(range(X.shape[2])):
+                save_cols = X[:,:,top10].copy()
+                #-----zero2one-----
+                X[:,:,top10] = 1
+                pred1 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+                mean_pred1 = np.mean(pred1)
+                #-----one2zero-----
+                X[:,:,top10] = 0
+                pred2 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+                mean_pred2 = np.mean(pred2)
+                result.append({'feature_index' : i, 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
+                            'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
+            df = pd.DataFrame(result).sort_values('lambda0', ascending=False)
+            print(df)
+            top10.append(df.feature_index[n])
+        else:
+            for i in tqdm(range(X.shape[2])):
+                save_cols = X[:,:,i].copy()
+                #-----zero2one-----
+                X[:,:,i] = 1
+                pred1 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+                mean_pred1 = np.mean(pred1)
+                #-----one2zero-----
+                X[:,:,i] = 0
+                pred2 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+                mean_pred2 = np.mean(pred2)
 
-#             result.append({'feature_index' : i, 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
-#                            'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
+                result.append({'feature_index' : i, 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
+                            'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
 
-#         df = pd.DataFrame(result).sort_values('lambda0', ascending=False)
-#         top10.append(df.feature_index[0])
+            df = pd.DataFrame(result).sort_values('lambda0', ascending=False)
+            top10.append(df.feature_index[0])
 
-# with open('./data/Sequential_top10.txt', 'w') as file:
-#     file.write(top10)
+
+    #-----df저장-----
+    df.to_csv('./data/Method_Sequential_FI.csv', index=False)
+    #-----top10 feature만 저장-----
+    top10_list = list(map(lambda x:str(COLS[x]), top10))
+    print(top10_list)
+    with open('./data/Sequential_top10.txt', 'w') as file:
+        file.write(top10_list)
---
+++ 12_Method_allfit_eel.py
@@ -0,0 +1,106 @@
+import numpy as np
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+# from tensorflow.keras.models import Sequentiasl
+# from tensorflow.keras.layers import Dense, LSTM
+# from tensorflow.keras.layers import SimpleRNN
+# from tensorflow.keras.layers import Dropout, InputLayer, Activation
+# from tensorflow.keras.callbacks import EarlyStopping
+# from tensorflow.keras.regularizers import l2
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.compat.v1.keras.layers import CuDNNLSTM
+from sklearn import metrics 
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+#GPU 사용 설정, -1이면 CPU 사용
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+#https://www.tensorflow.org/guide/gpu#allowing_gpu_memory_growth
+#프로세스의 요구량만큼 메모리 사용 설정
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+if gpus:
+    try:
+        for i in range(len(gpus)):
+            tf.config.experimental.set_memory_growth(gpus[i], True)
+    except RuntimeError as e:
+        # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
+        print(e)
+
+import random    
+seed_num = 42
+random.seed(seed_num)
+#-----데이터 로드-----
+X = np.load('/project/LSH/x_(7727,10,4068).npy')
+y = np.load('/project/LSH/y_(7727,1).npy')
+#-----Item name 로드-----
+
+COLS = list(pd.read_csv('/project/LSH/total_data_7727.csv')['ITEMID'].sort_values().unique())
+#-----각 feature의 Entropy를 기록한 dictionary 생성-----
+def entropy(ratio_list):
+    one_ratio, zero_ratio = ratio_list[0], ratio_list[1] 
+    return - ((one_ratio * (np.log2(one_ratio))) + (zero_ratio * (np.log2(zero_ratio))))
+
+X = np.load('/project/LSH/x_(7727,10,4068).npy')
+
+entropy_dict = {}
+for i in tqdm(range(len(COLS))):
+    one_ratio = X[:,:,i].sum() / (X.shape[0]*X.shape[1])
+    zero_ratio = 1 - one_ratio
+    entropy_num = entropy([one_ratio, zero_ratio])
+    entropy_dict[COLS[i]] = entropy_num
+
+
+# #-----FI 측정 시작-----
+# with tf.device('/device:GPU:1'): 
+#     #-----모델 로드-----
+#     model = tf.keras.models.load_model('./models/ALLFIT_17-0.7645.hdf5')
+#     result = []
+#     for i in tqdm(range(X.shape[2]), desc="zero, one Predict"):
+#         save_cols = X[:,:,i].copy()
+#         #-----zero2one-----
+#         X[:,:,i] = 1
+#         pred1 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+#         mean_pred1 = np.mean(pred1)
+#         #-----one2zero-----
+#         X[:,:,i] = 0
+#         pred2 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+#         mean_pred2 = np.mean(pred2)
+#         result.append({'feature' : str(COLS[i]), 'one2zero' : mean_pred2,'zero2one' : mean_pred1,
+#                         'lambda0' : mean_pred2 - mean_pred1, 'lambda1' : (mean_pred2 - mean_pred1) * entropy_dict[COLS[i]]})
+#     df = pd.DataFrame(result).sort_values('lambda0', ascending=False)
+#     print(df)
+
+# #-----df저장-----
+# df.to_csv('./data/Method_eel_FI.csv', index=False)
+
+
+#-----FI 측정 시작-----
+with tf.device('/device:GPU:1'): 
+    #-----모델 로드-----
+    model = tf.keras.models.load_model('./models/ALLFIT_17-0.7645.hdf5')
+    result = []
+    for i in tqdm(range(X.shape[2]), desc="zero, one Predict"):
+        save_cols = X[:,:,i].copy()
+        #-----zero2one-----
+        X[:,:,i] = 1
+        pred1 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+        mean_pred1 = np.mean(pred1)
+        #-----one2zero-----
+        X[:,:,i] = 0
+        pred2 = model.predict(X, batch_size=10000, workers=-1, use_multiprocessing=True)
+        mean_pred2 = np.mean(pred2)
+        result.append({'feature' : str(COLS[i]), 'zero2one' : mean_pred1, 'one2zero' : mean_pred2,
+                        'lambda0' : mean_pred1 - mean_pred2, 'lambda1' : (mean_pred1 - mean_pred2) * entropy_dict[COLS[i]]})
+    df = pd.DataFrame(result).sort_values('lambda0', ascending=False)
+    print(df)
+
+#-----df저장-----
+df.to_csv('./data/Method_eel_FI_again.csv', index=False)
