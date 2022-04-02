502200643669a5559166db1bf924db34341d6d8d
suhyun <myksh0903@naver.com>
2022-04-02 21:17:02 +0900

    Adaboost-LSTM py 파일 수정

--- 한국정보과학회/adaboost_LSTM_custom.py
+++ 한국정보과학회/adaboost_LSTM_custom.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -21,17 +22,24 @@ import random
 from tensorflow.keras.callbacks import EarlyStopping
 
 seed_num = 42
+tf.random.set_seed(seed_num)
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# print(gpus)
+# if gpus:
+#     try:
+#         for i in range(len(gpus)):
+#             tf.config.experimental.set_memory_growth(gpus[i], True)
+#     except RuntimeError as e:
+#         # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
+#         print(e)
+# gpus = tf.config.experimental.list_physical_devices('GPU')
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-print(gpus)
-if gpus:
-    try:
-        for i in range(len(gpus)):
-            tf.config.experimental.set_memory_growth(gpus[i], True)
-    except RuntimeError as e:
-        # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
-        print(e)
-
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# print(gpus)
+# if gpus:  # gpu가 있다면, 용량 한도를 5GB로 설정
+#     tf.config.experimental.set_virtual_device_configuration(gpus[1], 
+#                                                             [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10*1024)])
 seed_num = 42
 random.seed(seed_num)
 
@@ -60,13 +68,13 @@ def get_model():
     lstm.add(Dense(units=1, activation='sigmoid'))
 
     lstm.compile(optimizer= keras.optimizers.Adam(learning_rate = 0.001), 
-                          loss = "binary_crossentropy", metrics=['acc'])
+                 loss = "binary_crossentropy", metrics=['acc'])
     return lstm
 
 from keras.wrappers.scikit_learn import KerasClassifier
-
+import random
 class MyKerasClassifier(KerasClassifier):
-    def fit(self, x, y, sample_weight=None, **kwargs):
+    def fit(self, x, y, sample_weight=None, validation_split=0.25, **kwargs):
         y = np.array(y)
         if len(y.shape) == 2 and y.shape[1] > 1:
             self.classes_ = np.arange(y.shape[1])
@@ -80,10 +88,19 @@ class MyKerasClassifier(KerasClassifier):
         if sample_weight is not None:
             print('sample weight : ', sample_weight)
             if sample_weight[0] == 0.00016175994823681658:
-#                 kwargs['sample_weight'] = sample_weight
-                print('x, y', x.shape, x.sum().sum())
-                return super().fit(x, y)
-            weights = sample_weight / sum(sample_weight)
+                return super(MyKerasClassifier, self).fit(x, y, **kwargs)
+            
+            idx = np.arange(len(x))
+            random.shuffle(idx)
+            i = int(len(x)*0.75)
+            train_x, train_y = x[idx[:i],:,:], y[idx[:i]]
+            val_x, val_y = x[idx[i:],:,:], y[idx[i:]]
+            
+            
+            train_sw, val_sw = sample_weight[idx[:i]], sample_weight[idx[i:]]
+            train_sw, val_sw = train_sw/sum(train_sw)*len(train_sw), val_sw/sum(val_sw)*len(val_sw)
+            
+            weights = val_sw / sum(val_sw)
             random_range = [(sum(weights[:i]), sum(weights[:i])+weights[i]) if i!=0 else (0, weights[i]) for i in range(len(weights))]
             random_nums = [random.random() for _ in range(len(weights))]
             idx_list = []
@@ -92,51 +109,61 @@ class MyKerasClassifier(KerasClassifier):
                     if j[0] < i <= j[1]:
                         idx_list.append(random_range.index(j))
                         break
-            new_x = x[idx_list, :, :]
-            new_y = y[idx_list]
-#             kwargs['sample_weight'] = sample_weight
-            print(new_x.sum().sum())
-            print('new_x, new_y', new_x.shape, new_y.shape)
-            return super().fit(new_x, new_y)
-            # return super().fit(x, y, sample_weight=sample_weight)
+            new_val_x = val_x[idx_list, :, :]
+            new_val_y = val_y[idx_list]
+            
+            kwargs['validation_data'] = (new_val_x, new_val_y)
+            kwargs['sample_weight'] = train_sw
+            return super(MyKerasClassifier, self).fit(train_x, train_y, **kwargs)
         
-    def predict(self, x, **kwargs):
-        kwargs = self.filter_sk_params(Sequential.predict_classes, kwargs)
-        classes = self.model.predict_classes(x, **kwargs)
-        return self.classes_[classes].flatten()
-
-from tensorflow.keras.callbacks import ModelCheckpoint
-with tf.device('/device:GPU:0'):
-    early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)
-    cb_checkpoint = ModelCheckpoint(filepath='./models/adaboost_lstm1.h5', monitor='val_acc',
-                                    verbose=1, save_best_only=True)
-    base_estimator = MyKerasClassifier(build_fn=get_model, epochs=50, batch_size=128, validation_split=0.25, callbacks=[early_stop, cb_checkpoint])
+        return super(MyKerasClassifier, self).fit(x, y, **kwargs)
     
-    boosted_classifier = AdaBoostClassifier(base_estimator=base_estimator,n_estimators=3, random_state=42, learning_rate=0.8)
-    # print("Single LSTM Start")
-    # lstm.fit(X_train, y_train, epochs=50, batch_size=256, validation_split=0.2)
-    # single_preds = lstm.predict(X_test)
-    # single_preds[single_preds>0.5]=1
-    # single_preds[single_preds<=0.5]=0
-    print("Adaboost LSTM Start")
-    boosted_classifier.fit(X_train, y_train)
-    preds = boosted_classifier.predict(X_test)
+    
+    
+    def predict(self, x, **kwargs):
+        return super(MyKerasClassifier, self).predict(x)
 
-    # single_precision = precision_score(y_test, single_preds)
-    # single_recall = recall_score(y_test, single_preds)
-    # single_f1 = f1_score(y_test, single_preds)
-    # single_roc_auc = roc_auc_score(y_test, single_preds)
-    # single_acc = accuracy_score(y_test, single_preds)
-    # print(boosted_classifier.estimators_)
-    # print(boosted_classifier.estimators_[0].model_.get_weights()[0][0, :5])  # first sub-estimator
-    # print(boosted_classifier.estimators_[1].model_.get_weights()[0][0, :5])  # second sub-estimator
+# with tf.device('/device:GPU:1'):
+
+batch = 516
+estimators_nums = 200
+lr = 0.9
+
+early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)
+# base_estimator = MyKerasClassifier(build_fn=get_model, epochs=300, batch_size=batch,
+#                                     validation_split=0.25, callbacks=[early_stop])
+# boosted_classifier = AdaBoostClassifier(base_estimator=base_estimator, n_estimators=estimators_nums, random_state=42, learning_rate=lr)
+
+# print("Adaboost LSTM Start")
+# boosted_classifier.fit(X_train, y_train)
+# preds = boosted_classifier.predict(X_test)
+
+# precision = precision_score(y_test, preds)
+# recall = recall_score(y_test, preds)
+# f1 = f1_score(y_test, preds)
+# roc_auc = roc_auc_score(y_test, preds)
+# acc = accuracy_score(y_test, preds)
+
+# print(f'Adaboost accuracy : {acc}, precision : {precision}, recall : {recall}, f1 : {f1}, roc_auc : {roc_auc}')
+# acc_list = [accuracy_score(y_test,i) for i in boosted_classifier.staged_predict(X_test)]
+# print(acc_list)
+# import time
+# now = time.localtime()
+# nowtime = "%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+# with open('./Result_of_Adaboost.txt', 'a') as f:
+#     f.write(f'{nowtime} [batch:{batch}, n:{estimators_nums}, lr:{lr}]- accuracy : {acc}, precision : {precision}, recall : {recall}, f1 : {f1}, roc_auc : {roc_auc} acc_list : {acc_list} \n')
 
 
-    precision = precision_score(y_test, preds)
-    recall = recall_score(y_test, preds)
-    f1 = f1_score(y_test, preds)
-    roc_auc = roc_auc_score(y_test, preds)
-    acc = accuracy_score(y_test, preds)
+model = get_model()
+model.fit(X_train, y_train, epochs=300, batch_size=batch, validation_split=0.25, callbacks=[early_stop])
+preds = model.predict(X_test)
 
-    # print(f'single accuracy : {single_acc}, precision : {single_precision}, recall : {single_recall}, f1 : {single_f1}, roc_auc : {single_roc_auc}')
-    print(f'Adaboost accuracy : {acc}, precision : {precision}, recall : {recall}, f1 : {f1}, roc_auc : {roc_auc}')
+preds[preds>0.5]=1
+preds[preds<=0.5]=0
+precision = precision_score(y_test, preds)
+recall = recall_score(y_test, preds)
+f1 = f1_score(y_test, preds)
+roc_auc = roc_auc_score(y_test, preds)
+acc = accuracy_score(y_test, preds)
+
+print(f'accuracy : {acc}, precision : {precision}, recall : {recall}, f1 : {f1}, roc_auc : {roc_auc}')
